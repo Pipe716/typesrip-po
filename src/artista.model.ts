7bf758bf520fca82c93e3c1eb1b2db3aee041782
Pipe716 <pipe716@hotmail.com>
2016-10-29 15:49:51 -0500

    album clase creado

--- src/artista.model.ts
+++ src/artista.model.ts
@@ -1,4 +1,12 @@
-export class Artista
+ class Artista
 {
-    
+    public nombre: string;
+    private edad: number;
+
+    constructor () {}
+    public presentarse()
+    {
+        alert ("Hola soy:" + this.nombre);
+    }
+
 }
