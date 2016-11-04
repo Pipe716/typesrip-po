971c0b822936d680e7c996bc7ef8e0a620d8d4a3
Pipe716 <pipe716@hotmail.com>
2016-11-03 21:13:44 -0500

    Actualizacion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-var App = (function () {
-    function App() {
+var Aplicacion = (function () {
+    function Aplicacion() {
+        this.albumList = new Array();
     }
-    App.prototype.main = function () {
-        var artista1 = new Artista();
-        artista1.presentarse();
+    Aplicacion.prototype.agregarAlbum = function () {
+        var inputAlbumNombre = document.getElementById("album_nombre");
+        var inputAlbumFechaLanzamiento = document.getElementById("album_fechaLanzamiento");
+        var nombreAlbum = inputAlbumNombre.value;
+        var fechaLanzamiento = new Date(inputAlbumFechaLanzamiento.value);
+        var nuevoAlbum = new Album(nombreAlbum, fechaLanzamiento);
+        this.albumList.push(nuevoAlbum);
+        alert("Album agregado con éxito");
     };
-    return App;
+    Aplicacion.prototype.listarAlbums = function () {
+        var tablaAlbumsElement = document.getElementById("table_albums");
+        var htmlTablaAlbums = "<tr><th>Nombre</th><th>Fecha de lanzamiento</th></tr>";
+        for (var i = 0; i < this.albumList.length; i++) {
+            htmlTablaAlbums = htmlTablaAlbums +
+                "<tr>" +
+                "<td>" + this.albumList[i].nombre + "</td>" +
+                "<td>" + this.albumList[i].fechaLanzamiento + "</td>" +
+                "</tr>";
+        }
+        tablaAlbumsElement.innerHTML = htmlTablaAlbums;
+        tablaAlbumsElement.style.visibility = "visible";
+    };
+    return Aplicacion;
 }());
-alert("Se ha cargado la aplicacion");
+var app = new Aplicacion();
 //# sourceMappingURL=app.js.map
